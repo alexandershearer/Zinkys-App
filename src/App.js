063ae86b2a5f2d4356b926c774083ba5e6dcf4d3
Adrian Bragg <adrianbragg0@gmail.com>
2019-12-06 10:46:56 -0600

    Added navbar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Nav from 'react-bootstrap/Nav';
 function App() {
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="info">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="#home">
           <img id='gssLogo'
-            src="GSS.png"
-            width="100"
+            src="Zinkys.png"
+            width="75"
             height="75"
             className="d-inline-block align-top"
             alt="GSS logo"
@@ -19,10 +19,11 @@ function App() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#upcoming">Upcoming Events</Nav.Link>
-            <Nav.Link href="#past">Past Events</Nav.Link>
-            <Nav.Link href="#schedule">Schedule</Nav.Link>
-            <Nav.Link eventKey={2} href="#about">About</Nav.Link>
+            <Nav.Link href="#upcoming">Grooming</Nav.Link>
+            <Nav.Link href="#past">Doggy Daycare</Nav.Link>
+            <Nav.Link href="#schedule">Boarding</Nav.Link>
+            <Nav.Link eventKey={2} href="#about">Full Price List</Nav.Link>
+            <Nav.Link href="#schedule">Requirements</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>

4a95d5d18604f77b9923de714263132b55904128
Adrian Bragg <adrianbragg0@gmail.com>
2019-12-06 10:31:36 -0600

    Test push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="info">
+        <Navbar.Brand href="#home">
+          <img id='gssLogo'
+            src="GSS.png"
+            width="100"
+            height="75"
+            className="d-inline-block align-top"
+            alt="GSS logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#upcoming">Upcoming Events</Nav.Link>
+            <Nav.Link href="#past">Past Events</Nav.Link>
+            <Nav.Link href="#schedule">Schedule</Nav.Link>
+            <Nav.Link eventKey={2} href="#about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }

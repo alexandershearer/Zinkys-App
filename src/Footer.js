debe43564708f559ad09d34550104982b983fee3
Adrian Bragg <adrianbragg0@gmail.com>
2019-12-12 10:10:40 -0600

    Added a link to the address in the footer to be able to get directions to Zinky's

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
     render() {
         return (
             <div class="footer">
-                <div>
-                    <p>102 W Cherry St. Glasgow, KY 42141</p>
-                </div>
+                <a href='https://www.google.com/maps/dir/36.9803264,-85.9201536/102+W+Cherry+St,+Glasgow,+KY+42141/@36.9905984,-85.9275137,13.97z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x88663dbd47831089:0xd112a5b52bcdc094!2m2!1d-85.9087639!2d37.0011152'>
+                    <p className='addressName'>102 W Cherry St. Glasgow, KY 42141</p>
+                </a>
                 <div>
                     {/* <img class="fas fa-phone" style='font-size:20px;color:#007bff;margin: 5px;'></img> */}
                     <p>(270) 629-3436</p>
